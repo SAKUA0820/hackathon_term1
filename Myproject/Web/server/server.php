3f482a312dc9f9c5c289c4d06f919663985071d9
SAKUA0820 <asuka0820g10@gmail.com>
2023-07-08 23:08:05 +0900

    GETで受け取れるように変更

--- Myproject/Web/server/server.php
+++ Myproject/Web/server/server.php
@@ -17,27 +17,29 @@ define('DB_PASSWORD', 'hackathon1');
 // define('DB_PASSWORD', 'nemui');  //←高橋自宅の場合
 define('PDO_DSN', 'mysql:dbhost=localhost;dbname='.DB_DATABASE);
 
-try{
-    // json受け取り
-    $json = file_get_contents("php://input");
-    // 第4引数: エラー発生時に、JSONExceptionとする設定
-    $params = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
-}catch(JSONException $e){
-    // リクエストの形式がおかしい時
-    header("HTTP/1.1 400 Bad Request");
-    header("Content-Type: application/json; charset=utf-8");
-    $result = array('code'=>400, 'message'=>'リクエストデータの形式が正しくありません','description'=>$e->getMessage());
-    echo json_encode($result, JSON_UNESCAPED_UNICODE);
-    exit;
-}
+$method = $_GET['method'];
+
+// try{
+//     // json受け取り
+//     $json = file_get_contents("php://input");
+//     // 第4引数: エラー発生時に、JSONExceptionとする設定
+//     $params = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+// }catch(JSONException $e){
+//     // リクエストの形式がおかしい時
+//     header("HTTP/1.1 400 Bad Request");
+//     header("Content-Type: application/json; charset=utf-8");
+//     $result = array('code'=>400, 'message'=>'リクエストデータの形式が正しくありません','description'=>$e->getMessage());
+//     echo json_encode($result, JSON_UNESCAPED_UNICODE);
+//     exit;
+// }
 
 // DBアクセス 
     try {
         $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // selectAllMark (送られてきたjsonのmethodの値がselectAllMarkだった場合)
-        if($params[0]['method'] == 'selectAllMark') {
+        // selectAllMark (送られてきたmethodの値がselectAllMarkだった場合)
+        if($method == 'selectAllMark') {
             $stmt = $db->prepare("select * from mark_list");
             $stmt->execute();
 
@@ -51,10 +53,10 @@ try{
             return;
         }
 
-        // selectMark (送られてきたjsonのmethodの値がselectMarkだった場合)
-        if($params[0]['method'] == 'selectMark') {
+        // selectMark (送られてきたmethodの値がselectMarkだった場合)
+        if($method == 'selectMark') {
             $stmt = $db->prepare("select * from mark_list where id = :id");
-            $stmt->bindValue(':id', $params[0]['id'], PDO::PARAM_INT);
+            $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -67,8 +69,8 @@ try{
             return;
         }
 
-        // selectAllComment (送られてきたjsonのmethodの値がselectAllCommentだった場合)
-        if($params[0]['method'] == 'selectAllComment') {
+        // selectAllComment (送られてきたmethodの値がselectAllCommentだった場合)
+        if($method == 'selectAllComment') {
             $stmt = $db->prepare("select * from irritation_history");
             $stmt->execute();
 
@@ -82,11 +84,11 @@ try{
             return;
         }
 
-        // insertMark (送られてきたjsonのmethodの値がselectAllCommentだった場合)
-        if($params[0]['method'] == 'insertMark') {
+        // insertMark (送られてきたmethodの値がselectAllCommentだった場合)
+        if($method == 'insertMark') {
             $stmt = $db->prepare("insert into mark_list (width, height) value (:width, :height)");
-            $stmt->bindValue(':width', /*$postTest->width*/$params[0]['width'], PDO::PARAM_INT);
-            $stmt->bindValue(':height', /*$postTest->height*/$params[0]['height'], PDO::PARAM_INT);
+            $stmt->bindValue(':width', /*$postTest->width*/$_GET['width'], PDO::PARAM_INT);
+            $stmt->bindValue(':height', /*$postTest->height*/$_GET['height'], PDO::PARAM_INT);
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -95,12 +97,12 @@ try{
             return;
         }
 
-        // insertComment (送られてきたjsonのmethodの値がinsertCommentだった場合)
-        if($params[0]['method'] == 'insertComment') {
+        // insertComment (送られてきたmethodの値がinsertCommentだった場合)
+        if($method == 'insertComment') {
             $stmt = $db->prepare("insert into irritation_history (mark_id, comment, anger_point) value (:mark_id, :comment, :anger_point)");
-            $stmt->bindValue(':mark_id', $params[0]['mark_id'], PDO::PARAM_INT);
-            $stmt->bindValue(':comment', $params[0]['comment'], PDO::PARAM_STR);
-            $stmt->bindValue(':anger_point', $params[0]['anger_point'], PDO::PARAM_INT);
+            $stmt->bindValue(':mark_id', $_GET['mark_id'], PDO::PARAM_INT);
+            $stmt->bindValue(':comment', $_GET['comment'], PDO::PARAM_STR);
+            $stmt->bindValue(':anger_point', $_GET['anger_point'], PDO::PARAM_INT);
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -109,14 +111,14 @@ try{
             return;
         }
 
-        // updateMark (送られてきたjsonのmethodの値がupdateMarkだった場合)
-        if($params[0]['method'] == 'updateMark') {
+        // updateMark (送られてきたmethodの値がupdateMarkだった場合)
+        if($method == 'updateMark') {
             $stmt = $db->prepare("update mark_list set width = :width, height = :height, total_anger_point = :total_anger_point, burst_flag = :burst_flag where id = :id");
-            $stmt->bindValue(':width', $params[0]['width'], PDO::PARAM_INT);
-            $stmt->bindValue(':height', $params[0]['height'], PDO::PARAM_INT);
-            $stmt->bindValue(':total_anger_point', $params[0]['total_anger_point'], PDO::PARAM_INT);
-            $stmt->bindValue(':burst_flag', $params[0]['burst_flag'], PDO::PARAM_INT);
-            $stmt->bindValue(':id', $params[0]['id'], PDO::PARAM_INT);
+            $stmt->bindValue(':width', $_GET['width'], PDO::PARAM_INT);
+            $stmt->bindValue(':height', $_GET['height'], PDO::PARAM_INT);
+            $stmt->bindValue(':total_anger_point', $_GET['total_anger_point'], PDO::PARAM_INT);
+            $stmt->bindValue(':burst_flag', $_GET['burst_flag'], PDO::PARAM_INT);
+            $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -125,12 +127,12 @@ try{
             return;
         }
 
-        // insert or update (送られてきたjsonのmethodの値がsaveだった場合)
-        // if($params[0]['method'] == 'save') {
+        // insert or update (送られてきたmethodの値がsaveだった場合)
+        // if($method == 'save') {
         //     $stmt = $db->prepare("insert into test (name, height, width) value (:name, :height, :width) on duplicate key update height = values(height), width = values(width)");
-        //     $stmt->bindValue(':name', /*$postTest->name*/$params[0]['name'], PDO::PARAM_STR);
-        //     $stmt->bindValue(':height', /*$postTest->height*/$params[0]['height'], PDO::PARAM_INT);
-        //     $stmt->bindValue(':width', /*$postTest->width*/$params[0]['width'], PDO::PARAM_INT);
+        //     $stmt->bindValue(':name', /*$postTest->name*/$_GET['name'], PDO::PARAM_STR);
+        //     $stmt->bindValue(':height', /*$postTest->height*/$_GET['height'], PDO::PARAM_INT);
+        //     $stmt->bindValue(':width', /*$postTest->width*/$_GET['width'], PDO::PARAM_INT);
         //     $stmt->execute();
 
         //     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
